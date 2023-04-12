9a7adbcd01b6a569a956618197a48623f77391c4
Jakob Durstberger <jakob@durstberger.me>
2023-04-12 12:16:00 +0200

    [TELE-1621] export version before building jar
    Authors: @JDurstberger @jordanrobinson

--- pipelines/build/task.sh
+++ pipelines/build/task.sh
@@ -16,9 +16,9 @@ export GIT_SHA="$(git rev-parse --short HEAD)"
 
 mkdir -p "$PROJECT_DIR/build"
 cp "$ROOT_DIR/version/version" "$PROJECT_DIR/build/version"
+VERSION=$(./go version | tail -n 1)
 
 ./go library:build
 
-VERSION=$(./go version | tail -n 1)
-
+ls "build/libs"
 cp "build/libs/otel-extension-$VERSION-all.jar" "$ROOT_DIR/dist/"
